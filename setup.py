5d5061909dd9f1c9c92159ed0ad4b17bb46ff49c
samgill844 <samgill844@gmail.com>
2021-08-04 12:55:13 +0100

    Paper

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ import numpy
 ext_modules = [
     Extension(
         "cybinary",
-        ["source/kepler.pyx"],#, "source/rv_code.pyx"],
+        ["source/lctemplatematch.pyx"],#, "source/rv_code.pyx"],
     )
 ]
 
 setup(
-    name='cybinary',
+    name='lctemplatematch',
     ext_modules=cythonize(ext_modules),
     include_dirs=[numpy.get_include()],
     extra_compile_args=['-O3','-fopenmp'],
     extra_link_args=['-fopenmp'],
-    scripts=['Utils/tls']
+    scripts=['Utils/lctemplatematch']
 )
